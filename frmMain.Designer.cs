41d8445e16bad21e4f96569b55e4abaa7396318d
Ertan YILDIZ <ertanyildizz@gmail.com>
2017-12-23 08:57:36 +0300

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ServerMonitor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanelDevices = new System.Windows.Forms.TableLayoutPanel();
+            this.lblPingText = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanelDevices);
+            this.groupBox1.Controls.Add(this.lblPingText);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1008, 997);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Devices";
+            // 
+            // tableLayoutPanelDevices
+            // 
+            this.tableLayoutPanelDevices.ColumnCount = 2;
+            this.tableLayoutPanelDevices.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelDevices.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelDevices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelDevices.Location = new System.Drawing.Point(3, 27);
+            this.tableLayoutPanelDevices.Name = "tableLayoutPanelDevices";
+            this.tableLayoutPanelDevices.RowCount = 2;
+            this.tableLayoutPanelDevices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelDevices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelDevices.Size = new System.Drawing.Size(1002, 967);
+            this.tableLayoutPanelDevices.TabIndex = 6;
+            // 
+            // lblPingText
+            // 
+            this.lblPingText.AutoSize = true;
+            this.lblPingText.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold);
+            this.lblPingText.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.lblPingText.Location = new System.Drawing.Point(421, 0);
+            this.lblPingText.Name = "lblPingText";
+            this.lblPingText.Size = new System.Drawing.Size(102, 26);
+            this.lblPingText.TabIndex = 5;
+            this.lblPingText.Text = "pingText";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(1008, 997);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Ping Device";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPingText;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelDevices;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerMonitor
+{
+    public partial class frmMain : Form
+    {
+        private int columnCount = 4;
+
+
+        /// <summary>
+        /// device list
+        /// </summary>
+        public List<Device> Devices { get; set; }
+
+
+        /// <summary>
+        /// Ping result status
+        /// </summary>
+        public enum Status
+        {
+            Failed,
+            Success
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+            timer1.Interval = 30000;
+            timer1.Start();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lblPingText.Text = "";
+            CreateTable();
+            SendPing();
+
+        }
+        /// <summary>
+        /// Create buttons and place them in Table Layout Panel control.
+        /// Set each button's tag to a device object.
+        /// </summary>
+        private void CreateTable()
+        {
+            var deviceList = new List<Device>();
+            deviceList.Add(new Device() {Id = 1, Ip ="192.168.1.1", Label = "Main Server", Status = Status.Success });
+            deviceList.Add(new Device() {Id = 2, Ip ="192.168.1.2", Label = "Web Server", Status = Status.Success });
+            deviceList.Add(new Device() {Id = 3, Ip ="192.168.3.1", Label = "Camera 1", Status = Status.Success });
+            deviceList.Add(new Device() {Id = 4, Ip ="192.168.3.2", Label = "Camera 2", Status = Status.Success });
+            deviceList.Add(new Device() {Id = 5, Ip ="192.168.2.74", Label = "My Computer", Status = Status.Success });
+
+
+            Devices = deviceList;
+
+            tableLayoutPanelDevices.Controls.Clear();
+            tableLayoutPanelDevices.RowStyles.Clear();
+            tableLayoutPanelDevices.ColumnStyles.Clear();
+            tableLayoutPanelDevices.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+            tableLayoutPanelDevices.ColumnCount = deviceList.Count < 0 ? 1 : deviceList.Count;
+
+            tableLayoutPanelDevices.RowCount = 1;
+            for (int i = 0; i < 20; i++)
+            {
+                tableLayoutPanelDevices.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20));
+            }
+
+            tableLayoutPanelDevices.RowStyles.Add(new RowStyle(SizeType.Percent, 20));
+
+            foreach (var device in deviceList)
+            {
+                
+                var myButton = new Button();
+                myButton.Text = device.Label;
+                myButton.Name = $"b_{device.Id}";
+                myButton.Tag = device;
+                myButton.Click += b_Click;
+                myButton.Dock = DockStyle.Fill;
+                myButton.FlatStyle = FlatStyle.Popup;
+                myButton.BackColor = Color.BurlyWood;
+                myButton.Font = new Font(myButton.Font.FontFamily, 16);
+                tableLayoutPanelDevices.Controls.Add(myButton);
+            }
+            
+        }
+
+        void b_Click(object sender, EventArgs e)
+        {
+            var pingNumber = 1;
+            
+            var b = sender as Button;
+            Device device = null;
+            var backColor = b.BackColor;
+            try
+            {
+                device = (Device)b.Tag;
+                b.Tag = device;
+                b.BackColor = Color.DarkOrange;
+                var sor = MessageBox.Show($"Send ping to device? ({device.Label})", "", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+                if (sor == DialogResult.Yes)
+                {
+                    var ping = Ping(device.Ip, pingNumber);
+                    if (ping)
+                    {
+                        b.BackColor = Color.GreenYellow;
+                        return;
+                    }
+                    b.BackColor = Color.Red;
+                    var notification = new NotifyIcon()
+                    {
+                        Visible = true,
+                        Icon = SystemIcons.Exclamation,
+                        BalloonTipTitle = "Failed",
+                        BalloonTipText = device.Label,
+                    };
+
+                    notification.ShowBalloonTip(3000);
+
+
+                    notification.Dispose();
+                }
+                else
+                {
+                    b.BackColor = backColor;
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        /// <summary>
+        /// Send ping to given ip adress.
+        /// </summary>
+        /// <param name="ip">Ip adress of the host.</param>
+        /// <param name="pingNumber">Number of ping sending.</param>
+        /// <returns></returns>
+        public bool Ping(string ip, int pingNumber)
+        {
+
+            var pingable = false;
+            using (Ping pinger = new Ping())
+            {
+                try
+                {
+                    for (int i = 0; i < pingNumber; i++)
+                    {
+                        PingReply reply = pinger.Send(ip);
+                        if (reply != null) pingable = reply.Status == IPStatus.Success;
+                        if (!pingable) continue;
+                        break;
+
+                    }
+
+                }
+                catch (PingException)
+                {
+                }
+            }
+            return pingable;
+        }
+
+
+        /// <summary>
+        /// Call SendPing method when timer ticks
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendPing();
+        }
+        /// <summary>
+        /// Check each devices in the Devices. Send ping to each devices.
+        /// Set the button color to ping result (yellow or red).
+        /// </summary>
+        private async void SendPing()
+        {
+            lblPingText.Text = "Sending ping...";
+            if (Devices.Count > 0)
+            {
+                foreach (var device in Devices)
+                {
+                    if (device == null) continue;
+
+                    var btnControls = Controls.Find($"b_{device.Id}", true);
+                    var btn = btnControls[0];
+                    btn.BackColor = Color.Gold;
+                    var tempText = btn.Text;
+                    btn.Text = "...";
+                    Task<bool> task = Task.Run(() => Ping(device.Ip, 1));
+                    device.Status = await task ? Status.Success : Status.Failed;
+                    btn.BackColor = device.Status == Status.Success ? Color.GreenYellow : Color.Red;
+                    btn.Tag = device;
+                    btn.Text = tempText;
+                }
+            }
+
+            lblPingText.Text = $"Latest ping sent date: {DateTime.Now.ToString("HH:mm")}";
+        }
+    
+    }
+
+    public class Device
+    {
+        public string Ip { get; set; }
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public frmMain.Status Status { get; set; }
+
+    }
+}
